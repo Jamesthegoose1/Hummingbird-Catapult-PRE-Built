afa89254235cdb2b119ed4b02576d2e7296b70b1
Dexional_RBLX <jbmorin2016@gmail.com>
2025-12-12 18:23:37 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.pause(2000)
+    hummingbird.setPositionServo(FourPort.One, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    hummingbird.setPositionServo(FourPort.One, 90)
+})
+hummingbird.startHummingbird()
 basic.forever(function () {
 	
 })
